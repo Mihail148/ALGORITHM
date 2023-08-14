326dc180ed6302a5fb08edd532ddeaeabed2b98b
Mihail Rkitcky <papi.pay@mail.ru>
2023-08-14 21:46:27 +0300

    Seminar3

---
+++ seminar3.py
@@ -0,0 +1,42 @@
+import hashlib
+
+class Node:
+    
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+    
+    def __str__(self):
+        return f"{self.data=} {self.next=}"
+            
+
+        
+class LinkedList:
+    
+    def __init__(self, head = None):
+        self.head = head
+    
+    def add_node(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    
+    def print_lst(self):
+        current = self.head
+        while current.next != None:
+            print(current)
+            current = current.next 
+        print(current)       
+        
+lst = LinkedList()
+lst.add_node("1")
+lst.add_node("2")
+lst.add_node("3")
+lst.add_node("4")
+
+lst.print_lst()
+
+
+
+        
+        
+                 
